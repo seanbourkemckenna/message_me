8f49cb8844d0cbddaf56cc57bf8422db8363b68d
Sean Bourke Mckenna <sean.bourkemckenna@intercom.io>
2020-03-19 10:58:50 +0000

    added user and message models, ability to login/logout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+User.create(username: "Sean", password: "derp")
+User.create(username: "Jason", password: "derp")
+User.create(username: "Mary", password: "derp")
+User.create(username: "Gale", password: "derp")
